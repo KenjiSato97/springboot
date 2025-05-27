411cc90813d1a04c760037e2fcad4b9fe9265629
Kenji Aranha Sato <30627646+KenjiSato97@users.noreply.github.com>
2025-05-27 19:58:38 -0300

    Adição por upload
    Adiçao dos arquivos por upload, desviando do problema da ide subindo apenas para a master

---
+++ springboot/src/main/java/com/example/springboot/controllers/ProductController.java
@@ -0,0 +1,73 @@
+package com.example.springboot.controllers;
+
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.services.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.create(productRecordDto));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.findAll());
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable UUID id) {
+        var product = productService.findById(id);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product);
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable UUID id, @RequestBody @Valid ProductRecordDto productRecordDto) {
+        var updatedProduct = productService.update(id, productRecordDto);
+        if (updatedProduct == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable UUID id) {
+        if (!productService.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        productService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+    }
+
+    @PutMapping("/products/{id}/quantity/{quantity}")
+    public ResponseEntity<Object> updateProductQuantity(@PathVariable UUID id, @PathVariable Integer quantity) {
+        var productOptional = productService.findById(id);
+        if (productOptional == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        productOptional.setQuantity(quantity);
+        return ResponseEntity.status(HttpStatus.OK).body(productService.update(id, new ProductRecordDto(
+                productOptional.getName(),
+                productOptional.getValue(),
+                quantity
+        )));
+    }
+}
---
+++ springboot/src/main/java/com/example/springboot/services/ProductService.java
@@ -0,0 +1,16 @@
+package com.example.springboot.services;
+
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductService {
+
+    ProductModel create(ProductRecordDto productDto);
+    List<ProductModel> findAll();
+    ProductModel findById(UUID id);
+    boolean existsById(UUID id);
+    ProductModel update(UUID id, ProductRecordDto productDto);
+    void delete(UUID id);
+}
